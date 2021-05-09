6c6795b026ace802ea53e61fa2e24d0853467a96
thanakorn-11911 <thanakorn.oau11911@gmail.com>
2021-05-09 00:27:20 -0700

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color primaryTextColor = Color(0xFF414C6B);
+Color secondaryTextColor = Color(0xFFE4979E);
+Color titleTextColor = Colors.white;
+Color contentTextColor = Color(0xff868686);
+Color navigationColor = Color(0xFF004D40);
+Color gradientStartColor = Color(0xFF006064);
+Color gradientEndColor = Color(0xFFA7FFEB);
